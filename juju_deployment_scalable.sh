01256358982c2a9bb7ef3df51ac4bc4402343afe
Kelley Parker <kelleyparker@users.noreply.github.com>
2022-03-01 16:10:57 -0600

    Update juju_deployment_scalable.sh

--- juju_deployment_scalable.sh
+++ juju_deployment_scalable.sh
@@ -7,8 +7,9 @@
 
 sudo apt-get install juju -y #if this fails, will use snap
 snap install juju --classic # Install juju, must use --classic
-juju bootstrap # Create new controller
-
+juju bootstrap localhost # Create new controller, use localhost, not a cloud provider - will take a few minutes, be patient
 juju deploy cs:landscape-scalable
 #^ After this deploy command finishes, wait _several_ minutes for this to set up.  Check up periodically by typing in 'juju status'.  
-# The IP next to juju status haproxy is the IP you'll use to interface with Landscape server on a web browser.
+# The IP next to juju status haproxy is the IP you'll use to interface with Landscape server on a web browser.  'juju status haproxy' will give you the 
+# container with the IP only and no others.
+juju status
