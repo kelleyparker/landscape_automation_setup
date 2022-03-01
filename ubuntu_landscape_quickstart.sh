f4f0a92cb661cabb8e73cd8e6b3b22a4e3faa071
Kelley Parker <kelleyparker@users.noreply.github.com>
2022-03-01 15:52:28 -0600

    Update ubuntu_landscape_quickstart.sh

--- ubuntu_landscape_quickstart.sh
+++ ubuntu_landscape_quickstart.sh
@@ -1,1 +1,3 @@
-
+#!/bin/bash
+# Instructions for Ubuntu Landscape Quickstart deployments are located here: https://docs.ubuntu.com/landscape/en/landscape-install-quickstart
+sudo add-apt-repository ppa:landscape/19.10 && sudo apt-get install landscape-server-quickstart
