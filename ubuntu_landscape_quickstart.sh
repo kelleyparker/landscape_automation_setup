0225d30647de69b6e738ef925c7cb354c64b18f5
Kelley Parker <kelleyparker@users.noreply.github.com>
2022-03-01 16:02:26 -0600

    Update ubuntu_landscape_quickstart.sh

--- ubuntu_landscape_quickstart.sh
+++ ubuntu_landscape_quickstart.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+# *** SCRIPT DEVELOPMENT STILL IN PROGRESS AS OF MARCH 2, 2022*** *
+
 # Instructions for Ubuntu Landscape Quickstart deployments are located here: https://docs.ubuntu.com/landscape/en/landscape-install-quickstart
+# This script is to be run only on supported versions of Ubuntu Landscape Server.  As of the writing of this script (2022-03-01), Ubuntu Landscape Server is supported on 18.04 and lower. 
 
-
-sudo add-apt-repository ppa:landscape/19.10 && sudo apt-get install landscape-server-quickstart
+sudo add-apt-repository ppa:landscape/19.10 && sudo apt-get install landscape-server-quickstart -y
 
 #################
 # Prompt for licensing.  (L) for licensed, (F) for free
@@ -10,5 +13,9 @@ sudo add-apt-repository ppa:landscape/19.10 && sudo apt-get install landscape-se
 # 
 echo "Are you licensing Ubuntu Landscape Server or are you using a free license?"
 echo "Licensed (L) - Free (F)"
-read licenseResponse
+read licenseResponse # Will need to continue on this for the license file location
 
+
+#################
+# This updates apt-get and installs the landscape-client on the server
+sudo apt-get update && sudo apt-get install landscape-client -y
